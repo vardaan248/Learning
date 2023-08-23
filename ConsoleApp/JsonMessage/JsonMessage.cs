636c082f6315691dc3bf6dc4a59552eeded2a93f
Vardaan <vardaansethi007@gmail.com>
2023-08-24 00:41:25 +0530

    Adding code to decode infinite json string having messages

---
+++ ConsoleApp/JsonMessage/JsonMessage.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace ConsoleApp.JsonMessage
+{
+    public class JsonMessage
+    {
+        public string GetJsonDeserializedString(string str)
+        {
+            str = "abcd {'message' : \"xyz {'message':'def'}\"}";
+            string result = string.Empty;
+
+            while (str != string.Empty)
+            {
+                var idx = str.IndexOf("{");
+                if (idx is not -1)
+                {
+                    var substr = str[..idx];
+                    result += substr;
+                    str = str[idx..];
+                    str = (JsonConvert.DeserializeObject<MessageModel>(str)).Message; // Deserialze the message
+                }
+                else
+                {
+                    result += str;
+                    str = string.Empty;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp/JsonMessage/MessageModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleApp.JsonMessage
+{
+    public class MessageModel
+    {
+        public string Message { get; set; }
+    }
+}
